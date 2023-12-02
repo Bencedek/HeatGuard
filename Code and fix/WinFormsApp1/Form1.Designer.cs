1ca5aa6cf09a97e22022d7652553aa2dbaf67919
AndRe4z <109996900+AndRe4z@users.noreply.github.com>
2023-12-02 19:52:05 +0100

    Add files via upload

---
+++ Code and fix/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Gépüzemeltető",
+            "Gyártástervező mérnök",
+            "Műszakvezető"});
+            this.comboBox2.Location = new System.Drawing.Point(114, 46);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(193, 28);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(255, 197);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 24);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Riasztás";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(168, 263);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Riasztás törlése";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Riasztási határérték:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(536, 393);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Button button1;
+        private RadioButton radioButton1;
+        private NumericUpDown numericUpDown1;
+        private Button button2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Code and fix/WinFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0: //Gépüzemeltető
+                    label1.Visible = true;
+                    radioButton1.Visible = true;
+                    numericUpDown1.Visible = false;
+                    button2.Visible = false;
+                    label3.Visible = false;
+                    break;
+                case 1: //Gyártástervező mérnök
+                    label1.Visible = false;
+                    radioButton1.Visible = false;
+                    numericUpDown1.Visible = true;
+                    button2.Visible = false;
+                    label3.Visible = true;
+                    break; 
+                case 2: //Műszakvezető
+                    label1.Visible = false;
+                    radioButton1.Visible = true;
+                    numericUpDown1.Visible = false;
+                    button2.Visible = true;
+                    label3.Visible = false;
+                    break;
+
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            radioButton1.Visible = false;
+            numericUpDown1.Visible = false;
+            button2.Visible = false;
+            label3.Visible = false;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            label1.Text = homerseklet.ToString();
+            comboBox2.SelectedIndex = 0;
+            numericUpDown1.Value = hatarertek;
+            radioButton1.AutoCheck = false;
+        }
+
+        bool futagep = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(futagep)
+            {
+                futagep = false;
+                button1.Text = "Start";
+            }
+            else
+            {
+                futagep = true;
+                button1.Text = "Stop";
+            }
+        }
+        int homerseklet = 20;
+        int hatarertek = 30;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (futagep)
+            {
+                homerseklet++;
+            }
+            else if(homerseklet>20)
+            {
+                homerseklet--;
+            }
+            label1.Text = homerseklet.ToString();
+            if(homerseklet >= hatarertek)
+            {
+                radioButton1.Checked = true;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            hatarertek = (int)numericUpDown1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+        }
+    }
+}
